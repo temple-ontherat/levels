59a6af8b877703ba1e06d78dbb3f5731fe680a3f
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2023-03-04 21:57:05 -0500

    Create sketch1.js
    adding a first sketch.

---
+++ sketch1.js
@@ -0,0 +1,14 @@
+let goat="";
+function setup() {
+  createCanvas(400, 400);
+  button=createButton("Press to go to next sketch");
+  button.position(100,100);
+  button.mousePressed(gotoSketch);
+}
+
+function draw() {
+  background(220);
+}
+function gotoSketch() {
+  location.replace("https://temple-ontherat.github.io/levels/sketch2.html")
+}
