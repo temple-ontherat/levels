b7dc1c07c003ddb43cbcbcd2e198aca00b1a56a8
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2023-03-04 22:57:42 -0500

    Create sketch3.js

---
+++ sketch3.js
@@ -0,0 +1,13 @@
+let radius=0
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  ellipse(200,200, radius);
+  if (radius<200) {
+     radius++;
+  }
+  else {  location.replace("https://temple-ontherat.github.io/levels/index1.html")}
+}
