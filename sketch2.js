53dd8ea10af137b05085b5e883d3969c98549a9c
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2023-03-04 22:02:11 -0500

    Create sketch2.js

---
+++ sketch2.js
@@ -0,0 +1,18 @@
+let x=0;
+let y=0;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  ellipse(x,y,20);
+  x++;
+  y++;
+  if (x==width) {
+    x=0;
+  }
+   if (y==height) {
+    y=0;
+  }
+}
